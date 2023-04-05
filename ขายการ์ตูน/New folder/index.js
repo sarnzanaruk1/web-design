7850210bdb895e3c426ff2f918871dff3b1b0511
sarnzanaruk1 <sarn.zaza1112233@gmail.com>
2023-04-05 21:49:07 +0700

    first commit

---
+++ ขายการ์ตูน/New folder/index.js
@@ -0,0 +1,71 @@
+let cartoon = [
+
+    {
+        id: 1,
+        name: "saitama",
+        link: "saitama.html"
+    },
+
+    {
+        id: 2,
+        name: "doraemon",
+        link: "doraemon.html"
+    },
+
+    {
+        id: 3,
+        name: "dragonball",
+        link: "dragonball.html"
+    },
+
+    {
+        id: 4,
+        name: "darling",
+        link: "darling.html"
+    },
+
+    {
+        id: 5,
+        name: "jujutsu",
+        link: "juju.html"
+    }
+]
+
+function search() {
+
+    let save = document.getElementById('tex1')
+
+    let save2 = save.value
+
+    if (save2 == 1 || save2 == "saitama") {
+        location.href = cartoon[0].link
+    }
+
+    if (save2 == 2 || save2 == "Doraemon") {
+        location.href = cartoon[1].link
+    }
+
+    if (save2 == 3 || save2 == "Dragonball") {
+        location.href = cartoon[2].link
+    }
+
+    if (save2 == 4 || save2 == "Darling") {
+        location.href = cartoon[3].link
+    }
+
+    if (save2 == 5 || save2 == "JUJUTSU") {
+        location.href = cartoon[4].link
+    }
+}
+
+function KeyEnter(even) {
+    if (even.keyCode == 13) {
+        search()
+    }
+}
+
+let r = confirm('Press a button!')
+if (r == true)
+    console.log('ok')
+else
+    console.log('cancle')
